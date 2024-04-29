0736880aa2212955efc95b67a8809814fecbb6dd
roksana35 <roksana7235@gmail.com>
2024-04-29 17:15:21 +0600

    add homepage bannar image

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -8,28 +8,28 @@ const Banner = () => {
        
         <div className="carousel w-full">
         <div id="slide1" className="carousel-item relative w-full">
-          <img src="https://i.ibb.co/G38JSjW/tranquil-scene-reflects-beauty-mountain-range-generative-ai.jpg" className="w-full" />
+          <img src="https://i.ibb.co/G38JSjW/tranquil-scene-reflects-beauty-mountain-range-generative-ai.jpg" className="w-full rounded-lg md:h-[550px]" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">❮</a> 
             <a href="#slide2" className="btn btn-circle">❯</a>
           </div>
         </div> 
         <div id="slide2" className="carousel-item relative w-full">
-          <img src="https://i.ibb.co/CKr5hzF/84558.jpg" className="w-full" />
+          <img src="https://i.ibb.co/CKr5hzF/84558.jpg" className="w-full rounded-lg md:h-[550px]" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide1" className="btn btn-circle">❮</a> 
             <a href="#slide3" className="btn btn-circle">❯</a>
           </div>
         </div> 
         <div id="slide3" className="carousel-item relative w-full">
-          <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+          <img src="https://i.ibb.co/j6Y5p1M/3d-abstract-beautiful-flowers.jpg" className="w-full md:h-[550px] rounded-lg" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" className="btn btn-circle">❮</a> 
             <a href="#slide4" className="btn btn-circle">❯</a>
           </div>
         </div> 
         <div id="slide4" className="carousel-item relative w-full">
-          <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+          <img src="https://i.ibb.co/vZrrWBf/elevated-view-human-hand-painting-sunset-seen-with-paint-brush-wooden-desk.jpg" className="w-full rounded-lg md:h-[550px]" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide3" className="btn btn-circle">❮</a> 
             <a href="#slide1" className="btn btn-circle">❯</a>
