d03dd547a999279be729f45b6ac38724deb43cbd
roksana35 <roksana7235@gmail.com>
2024-04-30 17:41:48 +0600

    some change of my extrasection and mycraftlist

--- src/Components/Extrasection/Extrasection2.jsx
+++ src/Components/Extrasection/Extrasection2.jsx
@@ -7,9 +7,9 @@ const Extrasection2 = () => {
                 <img src="https://i.ibb.co/XtQd4yR/mother-with-daughter-drawing-park.jpg" className="md:pt-8 rounded-2xl" alt="mother" />
             </div>
             <div className="md:w-[400px]">
-                <h1 className="text-xl font-bold mt-3 mb-2">Impressonist's Magic with Space & Color</h1>
+                <h1 className="text-xl font-bold mt-3 mb-2">Impressonist Magic with Space & Color</h1>
                 <p className="font-medium">
-Impressionism is like capturing fleeting moments of magic on canvas. It's the art of breathing life into space and color, where every stroke whispers secrets of light and atmosphere. Imagine standing amidst a sun-drenched field, the golden hues of wheat swaying gently in the breeze. The artist's brush dances across the canvas, translating the warmth of sunlight into vibrant yellows and oranges that seem to shimmer with movement. In the distance, a tranquil river meanders through the landscape, its serene blue reflecting the sky above.</p>
+Impressionism is like capturing fleeting moments of magic on canvas. It is the art of breathing life into space and color, where every stroke whispers secrets of light and atmosphere. Imagine standing amidst a sun-drenched field, the golden hues of wheat swaying gently in the breeze. The artist brush dances across the canvas, translating the warmth of sunlight into vibrant yellows and oranges that seem to shimmer with movement. In the distance, a tranquil river meanders through the landscape, its serene blue reflecting the sky above.</p>
             </div>
         </div>
     );
